a6c97da195f773452fbd6e49cfc3a1ca27de699c
Nicolas <nicolas.velho@etec.sp.gov.br>
2017-03-15 13:03:36 -0300

    Php e bootstrap finalizado

--- index.php
+++ index.php
@@ -1,26 +1,39 @@
 <?php
 
+if (isset($_POST['enviar'])) {
 $valor1 = $_POST["valor1"];
 $valor2 = $_POST["valor2"];
-$resultado = $valor1 + $valor2
-echo $resultado;
-
-
+$resultado = $valor1 + $valor2;
+}
 
 ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <title></title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/style.css" rel="stylesheet">
+        <link href="bootstrap.min.css" rel="stylesheet">
     </head>
+
     <body>
-    <form>
-     Valor 1<input type="text" name="valor1" value="valor1">
-     Valor 2<input type="text" name="valor2" value="valor2">   
-     <input type="submit" name="enviar" value="enviar">
+    
+    <form method="post" action="">
+     Valor 1<input type="text" name="valor1" value="">
+     Valor 2<input type="text" name="valor2" value="">   
+      <a href="" class="btn btn-enviar">Enviar</a>
+    <br/>
+     
+     <?php
+     if (isset($resultado)) {
+     echo "A soma é: $resultado";
+     } ?>
+    
+
     </form>
+
     </body>
+
 </html>
